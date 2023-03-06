e09f1141b4552cfd97650a697f94204db1889c50
Shashank Siripragada <siripragada.s@northeastern.edu>
2023-03-06 05:13:45 -0500

    updates

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -36,10 +37,30 @@ class CausalSelfAttention(nn.Module):
         b, t, c = x.shape
 
         q, k, v = self.c_attn(x).split(self.n_embed, dim=2)
-        q = q.view(b, t, self.n_head, c // self.n_head)  # b, t, nh, hs
-        q = q.transpose(1, 2)
-        k = k.view(b, t, self.n_head, c // self.n_head)
-        k = k.transpose(1, 2)
+
+        q = q.view(b, t, self.n_head, c // self.n_head)  # b, t, num heads, head size
+        q = q.transpose(1, 2)  # b, num heads, t, head size
+
+        k = k.view(b, t, self.n_head, c // self.n_head)  # b, t, num heads, head size
+        k = k.transpose(1, 2)  # b, num heads, t, head size
+
+        v = v.view(b, t, self.n_head, c // self.n_head)  # b, t, num heads, head size
+        v = v.transpose(1, 2)  # b, num heads, t, head size
+
+        # dot product attention
+        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.shape[-1]))
+        att = att.masked_fill(self.tril[:, :, :t, :t] == 0, float('-inf'))
+        att = F.softmax(att, dim=-1)
+        att = self.attn_dropout(att)
+
+        y = att @ v  # b, num heads, t, head size
+
+        y = y.transpose(1, 2).contiguous().view(b, t, c)
+
+        y = self.resid_dropout(self.c_proj(y))
+
+        return y
+
 
 
 
--- tests/test_model.py
+++ tests/test_model.py
@@ -4,5 +4,5 @@ from model import CausalSelfAttention, GPTConfig
 
 
 class TestGPT(unittest.TestCase):
-
+    pass
 
